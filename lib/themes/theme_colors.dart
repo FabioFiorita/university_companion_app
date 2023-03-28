8ea0fde934fec0539743d7695f2dfaef33c1b959
Fabio Fiorita <fabiolfp@gmail.com>
2023-03-28 08:27:04 -0300

    Create theme_colors.dart

---
+++ lib/themes/theme_colors.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ThemeColors {
+  static const Color primary = Color.fromRGBO(75, 57, 239, 1.0);
+  static const Color secondary = Color.fromRGBO(251, 175, 124, 1.0);
+  static const Color tertiary = Color.fromRGBO(57, 210, 192, 1.0);
+  static const Color alternate = Colors.white;
+  static const Color primaryBackgroundLight = Color.fromRGBO(241, 244, 248, 1.0);
+  static const Color secondaryBackgroundLight = Colors.white;
+  static const Color primaryBackgroundDark = Color.fromRGBO(26, 31, 36, 1.0);
+  static const Color secondaryBackgroundDark = Color.fromRGBO(17, 20, 23, 1.0);
+  static const Color primaryTextLight = Color.fromRGBO(26, 31, 36, 1.0);
+  static const Color secondaryTextLight = Color.fromRGBO(139, 151, 162, 1.0);
+  static const Color primaryTextDark = Colors.white;
+  static const Color secondaryTextDark = Color.fromRGBO(139, 151, 162, 1.0);
+}
