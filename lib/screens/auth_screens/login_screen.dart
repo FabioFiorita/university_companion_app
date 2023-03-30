b1623a7f8df42792328ecae7f1fa8e264b15f24e
Fabio Fiorita <fabiolfp@gmail.com>
2023-03-30 11:10:11 -0300

    Create login_screen.dart

---
+++ lib/screens/auth_screens/login_screen.dart
@@ -0,0 +1,82 @@
+import 'package:c317_mobile/components/action_button.dart';
+import 'package:c317_mobile/components/input_textfield.dart';
+import 'package:c317_mobile/themes/color_schemes.dart';
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool _showPassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: ConstrainedBox(
+        constraints: BoxConstraints(
+          maxHeight: MediaQuery.of(context).size.height,
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              const Image(
+                image: AssetImage('assets/images/logo.png'),
+                height: 320,
+                fit: BoxFit.fitHeight,
+              ),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 4.0),
+                child: Text(
+                  'Login',
+                  style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                        color: lightColorScheme.primary,
+                      ),
+                ),
+              ),
+              Container(
+                height: 3,
+                width: 100,
+                color: lightColorScheme.primary,
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                child: InputTextField(
+                  isPassword: false,
+                  label: 'Entre com seu e-mail...',
+                  keyboardType: TextInputType.emailAddress,
+                ),
+              ),
+              const InputTextField(
+                isPassword: true,
+                label: 'Entre com sua senha...',
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                child: ActionButton(
+                  text: 'Entrar',
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/onboarding');
+                  },
+                  color: lightColorScheme.primary,
+                  textColor: lightColorScheme.onPrimary,
+                ),
+              ),
+              ActionButton(
+                text: 'Esqueci minha senha',
+                onPressed: () {
+                  Navigator.pushNamed(context, '/forgotPassword');
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    ));
+  }
+}
