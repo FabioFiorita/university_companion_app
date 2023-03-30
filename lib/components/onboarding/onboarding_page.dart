4dc3b8953e72ae7e142a7cf90aca4b3b8c769cfd
Fabio Fiorita <fabiolfp@gmail.com>
2023-03-30 12:21:19 -0300

    Create onboarding_page.dart

---
+++ lib/components/onboarding/onboarding_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+import '../../themes/color_schemes.dart';
+
+class OnboardingPage extends StatelessWidget {
+  IconData icon;
+  String title;
+  String description;
+  OnboardingPage({Key? key, required this.icon, required this.title, required this.description}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(
+              icon,
+              color: lightColorScheme.primary,
+              size: 200,
+            ),
+            Text(
+              title,
+              style: Theme.of(context).textTheme.titleLarge,
+              textAlign: TextAlign.center,
+            ),
+            Text(description,
+                style: Theme.of(context).textTheme.bodyLarge,
+                textAlign: TextAlign.center),
+          ],
+        ),
+      ),
+    );
+  }
+}
