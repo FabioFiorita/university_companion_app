5fbbf99c5788e053ea4cda3e62df50f1775b1a89
Fabio Fiorita <fabiolfp@gmail.com>
2023-03-30 11:10:18 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
-import 'package:c317_mobile/screens/login_screen.dart';
+import 'package:c317_mobile/screens/auth_screens/forgot_password_screen.dart';
+import 'package:c317_mobile/screens/auth_screens/login_screen.dart';
+import 'package:c317_mobile/screens/onboarding_screen.dart';
 import 'package:c317_mobile/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -17,6 +19,8 @@ class MyApp extends StatelessWidget {
       darkTheme: appThemeDark,
       routes: {
         '/login': (context) => const LoginScreen(),
+        '/forgotPassword': (context) => const ForgotPasswordScreen(),
+        '/onboarding': (context) => const OnboardingScreen(),
       },
       initialRoute: '/login',
     );
