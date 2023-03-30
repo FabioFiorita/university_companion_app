ba676a7f0b257e63d78f8068b84e40b1ff7df6f0
Fabio Fiorita <fabiolfp@gmail.com>
2023-03-30 11:10:28 -0300

    Create action_button.dart

---
+++ lib/components/action_button.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class ActionButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+  final Color? color;
+  final Color? textColor;
+
+  const ActionButton(
+      {Key? key,
+      required this.text,
+      required this.onPressed,
+        this.color,
+        this.textColor})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ConstrainedBox(
+      constraints: const BoxConstraints(
+        minWidth: 230,
+        minHeight: 50,
+      ),
+      child: ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          backgroundColor: color,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: Theme.of(context)
+              .textTheme
+              .bodyMedium
+              ?.copyWith(color: textColor),
+        ),
+      ),
+    );
+  }
+}
