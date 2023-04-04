043680487dcfd885b6d3d17e36c1ac8825b9fa73
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-04 17:47:20 -0300

    Update user_avatar.dart

--- lib/components/user_avatar.dart
+++ lib/components/user_avatar.dart
@@ -3,17 +3,23 @@ import 'package:flutter/material.dart';
 
 class UserAvatar extends StatelessWidget {
   String? imageUrl;
+
   UserAvatar({Key? key, this.imageUrl}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     if (imageUrl == null) {
       return CircleAvatar(
+        radius: 40,
         backgroundColor: lightColorScheme.primary,
-        child: const Icon(Icons.person),
+        child: const Icon(
+          Icons.person,
+          size: 40,
+        ),
       );
     } else {
       return CircleAvatar(
+        radius: 40,
         backgroundImage: NetworkImage(imageUrl!),
       );
     }
