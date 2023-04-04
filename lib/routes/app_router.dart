ae1e6ad605da8c57a96a0483f0e9e0e5a2af7802
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-04 17:47:17 -0300

    Update app_router.dart

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:beamer/beamer.dart';
+import 'package:c317_mobile/routes/navigation_screen.dart';
 import 'package:c317_mobile/screens/auth_screens/login_screen.dart';
 import 'package:c317_mobile/screens/home_screen.dart';
 import 'package:c317_mobile/screens/onboarding_screen.dart';
@@ -7,9 +8,23 @@ import '../screens/auth_screens/forgot_password_screen.dart';
 
 final appRouter = RoutesLocationBuilder(
   routes: {
-    '/': (context, state, data) => const LoginScreen(),
+    '/login': (context, state, data) => const LoginScreen(),
     '/forgot-password': (context, state, data) => const ForgotPasswordScreen(),
     '/onboarding': (context, state, data) => const OnboardingScreen(),
-    '/home': (context, state, data) => const HomeScreen(),
+    '/home': (context, state, data) {
+      int initialIndex = 0;
+      switch (state.queryParameters['tab']) {
+        case 'schedule':
+          initialIndex = 1;
+          break;
+        case 'grades':
+          initialIndex = 2;
+          break;
+        case 'profile':
+          initialIndex = 3;
+          break;
+      }
+      return NavigationScreen(initialIndex: initialIndex);
+    },
   },
 );
