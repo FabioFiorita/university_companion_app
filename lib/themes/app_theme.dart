0bdc5c7433cf4e94c92546776821b9c2d669c90b
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-04 17:47:11 -0300

    Update app_theme.dart

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -15,9 +15,15 @@ ThemeData appThemeDark = ThemeData(
 );
 
 TextTheme _textTheme = TextTheme(
-  titleLarge: GoogleFonts.poppins(fontSize: 28, fontWeight: FontWeight.w600),
-  titleMedium: GoogleFonts.poppins(fontSize: 22, fontWeight: FontWeight.w400),
-  titleSmall: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w200),
+  displayLarge: GoogleFonts.poppins(fontSize: 57, fontWeight: FontWeight.w600),
+  displayMedium: GoogleFonts.poppins(fontSize: 45, fontWeight: FontWeight.w600),
+  displaySmall: GoogleFonts.poppins(fontSize: 32, fontWeight: FontWeight.w600),
+  headlineLarge: GoogleFonts.poppins(fontSize: 32, fontWeight: FontWeight.w600),
+  headlineMedium: GoogleFonts.poppins(fontSize: 26, fontWeight: FontWeight.w600),
+  headlineSmall: GoogleFonts.poppins(fontSize: 24, fontWeight: FontWeight.w600),
+  titleLarge: GoogleFonts.poppins(fontSize: 22, fontWeight: FontWeight.w600),
+  titleMedium: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w400),
+  titleSmall: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w200),
   bodyLarge: GoogleFonts.poppins(fontSize: 18),
   bodyMedium: GoogleFonts.poppins(fontSize: 16),
   bodySmall: GoogleFonts.poppins(fontSize: 14),
