dbf00a3b2949fc85744f79267a7a7a66647ee03d
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-04 17:47:13 -0300

    Update home_screen.dart

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:c317_mobile/components/action_card.dart';
+import 'package:c317_mobile/components/class_card.dart';
+import 'package:c317_mobile/components/user_avatar.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -5,6 +8,59 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(24.0, 32.0, 24.0, 24.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            UserAvatar(),
+            Text(
+              'Bem vindo(a)',
+              style: Theme.of(context).textTheme.displaySmall,
+            ),
+            Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 8.0),
+                  child: Text(
+                    'Avner Joseph',
+                    style: Theme.of(context).textTheme.headlineMedium?.copyWith(
+                          color: Theme.of(context).colorScheme.primary,
+                        ),
+                  ),
+                ),
+                Icon(Icons.waving_hand,
+                    color: Theme.of(context).colorScheme.primary),
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+              child: Text('Sua próxima aula é',
+                  style: Theme.of(context).textTheme.bodyMedium),
+            ),
+            ClassCard(
+                subject: 'C317', date: 'Sexta-Feira - 19:30', location: 'I-15'),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 4.0),
+              child: ActionCard(
+                title: 'Ligar para a universidade',
+                description: 'Ver números da universidade para atendimento',
+                onPressed: () {},
+                icon: Icons.phone,
+                color: Theme.of(context).colorScheme.primary,
+              ),
+            ),
+            ActionCard(
+              title: 'E-mail dos professores',
+              description: 'Veja os e-mails dos professores para tirar dúvidas',
+              onPressed: () {},
+              icon: Icons.mail,
+              color: Theme.of(context).colorScheme.secondary,
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
