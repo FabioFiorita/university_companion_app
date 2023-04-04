f4a5c7d15a4e1048e797018a8724eefc17a15982
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-04 15:46:32 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,25 @@
-import 'package:c317_mobile/screens/auth_screens/forgot_password_screen.dart';
-import 'package:c317_mobile/screens/auth_screens/login_screen.dart';
-import 'package:c317_mobile/screens/onboarding_screen.dart';
+import 'package:beamer/beamer.dart';
+import 'package:c317_mobile/routes/app_router.dart';
 import 'package:c317_mobile/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final routerDelegate = BeamerDelegate(locationBuilder: appRouter);
+  MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return MaterialApp.router(
       title: 'UniApp',
       theme: appTheme,
       darkTheme: appThemeDark,
-      routes: {
-        '/login': (context) => const LoginScreen(),
-        '/forgotPassword': (context) => const ForgotPasswordScreen(),
-        '/onboarding': (context) => const OnboardingScreen(),
-      },
-      initialRoute: '/login',
+      routeInformationParser: BeamerParser(),
+      routerDelegate: routerDelegate,
+      backButtonDispatcher: BeamerBackButtonDispatcher(delegate: routerDelegate),
     );
   }
 }
