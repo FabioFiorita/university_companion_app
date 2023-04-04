b8beef43c1d3dfe55d6a77f2e9d5b4d9786555e3
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-04 17:47:24 -0300

    Create action_card.dart

---
+++ lib/components/action_card.dart
@@ -0,0 +1,76 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:c317_mobile/themes/color_schemes.dart';
+import 'package:flutter/material.dart';
+
+class ActionCard extends StatelessWidget {
+  final String title;
+  final String description;
+  final VoidCallback onPressed;
+  final IconData icon;
+  final Color color;
+
+  const ActionCard({
+    Key? key,
+    required this.title,
+    required this.description,
+    required this.onPressed,
+    required this.icon,
+    required this.color,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 120,
+      child: Card(
+        color: color,
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Row(
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(right: 8.0),
+                child: Icon(
+                  icon,
+                  size: 56,
+                  color: lightColorScheme.onPrimary,
+                ),
+              ),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    AutoSizeText(
+                      title,
+                      style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                            fontWeight: FontWeight.w600,
+                            color: lightColorScheme.onPrimary,
+                          ),
+                      maxLines: 2,
+                      minFontSize: 16,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 4.0, bottom: 4.0),
+                      child: AutoSizeText(
+                        description,
+                        style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                              color: lightColorScheme.onPrimary,
+                            ),
+                        maxLines: 2,
+                        minFontSize: 12,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+    ;
+  }
+}
