8e2af4a1d25d7684903bb36f7b376bf1b42a39af
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-06 12:15:45 -0300

    Create classes_schedule_screen.dart

---
+++ lib/screens/classes_schedule_screen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ClassesScheduleScreen extends StatelessWidget {
+  const ClassesScheduleScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Horarios'),
+      ),
+    );
+  }
+}
