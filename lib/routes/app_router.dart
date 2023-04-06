8d09643c309f52ed181cbc01fce9bcc51ba01074
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-06 12:15:31 -0300

    Update app_router.dart

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -1,30 +1,70 @@
-import 'package:beamer/beamer.dart';
-import 'package:c317_mobile/routes/navigation_screen.dart';
-import 'package:c317_mobile/screens/auth_screens/login_screen.dart';
+import 'package:c317_mobile/screens/classes_schedule_screen.dart';
 import 'package:c317_mobile/screens/home_screen.dart';
+import 'package:c317_mobile/screens/navigation_screen.dart';
+import 'package:c317_mobile/screens/auth_screens/login_screen.dart';
 import 'package:c317_mobile/screens/onboarding_screen.dart';
+import 'package:c317_mobile/screens/profile_screen.dart';
+import 'package:c317_mobile/screens/subjects_list_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 import '../screens/auth_screens/forgot_password_screen.dart';
 
-final appRouter = RoutesLocationBuilder(
-  routes: {
-    '/login': (context, state, data) => const LoginScreen(),
-    '/forgot-password': (context, state, data) => const ForgotPasswordScreen(),
-    '/onboarding': (context, state, data) => const OnboardingScreen(),
-    '/home': (context, state, data) {
-      int initialIndex = 0;
-      switch (state.queryParameters['tab']) {
-        case 'schedule':
-          initialIndex = 1;
-          break;
-        case 'grades':
-          initialIndex = 2;
-          break;
-        case 'profile':
-          initialIndex = 3;
-          break;
-      }
-      return NavigationScreen(initialIndex: initialIndex);
-    },
-  },
-);
+final _parentKey = GlobalKey<NavigatorState>(debugLabel: 'parent');
+final _shellKey = GlobalKey<NavigatorState>(debugLabel: 'shell');
+
+class AppRouter {
+  static final routerConfig = GoRouter(
+    initialLocation: '/login',
+    navigatorKey: _parentKey,
+    routes: [
+      GoRoute(
+        path: '/login',
+        parentNavigatorKey: _parentKey,
+        builder: (context, state) => const LoginScreen(),
+      ),
+      GoRoute(
+        path: '/forgot-password',
+        parentNavigatorKey: _parentKey,
+        builder: (context, state) => const ForgotPasswordScreen(),
+      ),
+      GoRoute(
+        path: '/onboarding',
+        parentNavigatorKey: _parentKey,
+        builder: (context, state) => const OnboardingScreen(),
+      ),
+      ShellRoute(
+        navigatorKey: _shellKey,
+        builder: (context, state, child) {
+          return NavigationScreen(child: child);
+        },
+        routes: [
+          GoRoute(
+            path: '/',
+            parentNavigatorKey: _shellKey,
+            pageBuilder: (context, state) =>
+                const NoTransitionPage(child: HomeScreen()),
+          ),
+          GoRoute(
+            path: '/classes',
+            parentNavigatorKey: _shellKey,
+            pageBuilder: (context, state) =>
+                const NoTransitionPage(child: ClassesScheduleScreen()),
+          ),
+          GoRoute(
+            path: '/subjects',
+            parentNavigatorKey: _shellKey,
+            pageBuilder: (context, state) =>
+                const NoTransitionPage(child: SubjectListScreen()),
+          ),
+          GoRoute(
+            path: '/profile',
+            parentNavigatorKey: _shellKey,
+            pageBuilder: (context, state) =>
+                const NoTransitionPage(child: ProfileScreen()),
+          ),
+        ],
+      ),
+    ],
+  );
+}
