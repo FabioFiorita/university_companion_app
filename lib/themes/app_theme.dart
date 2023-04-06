da1b6bf774a9993fcbb4694a9a30cbd8dc8a2b75
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-06 15:15:27 -0300

    Update app_theme.dart

--- lib/themes/app_theme.dart
+++ lib/themes/app_theme.dart
@@ -6,21 +6,22 @@ ThemeData appTheme = ThemeData(
   useMaterial3: true,
   colorScheme: lightColorScheme,
   textTheme: _textTheme,
-);
-
-ThemeData appThemeDark = ThemeData(
-  useMaterial3: true,
-  colorScheme: darkColorScheme,
-  textTheme: _textTheme,
+  scaffoldBackgroundColor: lightColorScheme.background,
 );
 
 TextTheme _textTheme = TextTheme(
-  displayLarge: GoogleFonts.poppins(fontSize: 48.0, fontWeight: FontWeight.w700),
-  displayMedium: GoogleFonts.poppins(fontSize: 34.0, fontWeight: FontWeight.w700),
-  displaySmall: GoogleFonts.poppins(fontSize: 24.0, fontWeight: FontWeight.w700),
-  headlineLarge: GoogleFonts.poppins(fontSize: 24.0, fontWeight: FontWeight.w700),
-  headlineMedium: GoogleFonts.poppins(fontSize: 20.0, fontWeight: FontWeight.w700),
-  headlineSmall: GoogleFonts.poppins(fontSize: 18.0, fontWeight: FontWeight.w700),
+  displayLarge:
+      GoogleFonts.poppins(fontSize: 48.0, fontWeight: FontWeight.w700),
+  displayMedium:
+      GoogleFonts.poppins(fontSize: 34.0, fontWeight: FontWeight.w700),
+  displaySmall:
+      GoogleFonts.poppins(fontSize: 24.0, fontWeight: FontWeight.w700),
+  headlineLarge:
+      GoogleFonts.poppins(fontSize: 24.0, fontWeight: FontWeight.w700),
+  headlineMedium:
+      GoogleFonts.poppins(fontSize: 20.0, fontWeight: FontWeight.w700),
+  headlineSmall:
+      GoogleFonts.poppins(fontSize: 18.0, fontWeight: FontWeight.w700),
   titleLarge: GoogleFonts.poppins(fontSize: 20.0, fontWeight: FontWeight.w500),
   titleMedium: GoogleFonts.poppins(fontSize: 18.0, fontWeight: FontWeight.w500),
   titleSmall: GoogleFonts.poppins(fontSize: 16.0, fontWeight: FontWeight.w500),
@@ -28,4 +29,3 @@ TextTheme _textTheme = TextTheme(
   bodyMedium: GoogleFonts.poppins(fontSize: 14.0, fontWeight: FontWeight.w400),
   bodySmall: GoogleFonts.poppins(fontSize: 12.0, fontWeight: FontWeight.w400),
 );
-
