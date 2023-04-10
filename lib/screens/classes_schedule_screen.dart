d688cdf6f7a977e1b14ef243b11a0d6ecb8c01eb
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-10 10:30:03 -0300

    Update classes_schedule_screen.dart

--- lib/screens/classes_schedule_screen.dart
+++ lib/screens/classes_schedule_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../components/class_card.dart';
+
 class ClassesScheduleScreen extends StatelessWidget {
   const ClassesScheduleScreen({Key? key}) : super(key: key);
 
@@ -8,6 +10,39 @@ class ClassesScheduleScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Horarios'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Minhas aulas',
+              style: Theme.of(context).textTheme.labelSmall,
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  ClassCard(
+                    subject: 'Matemática',
+                    date: 'Terça-feira 10:00 - 12:00',
+                    location: 'Aula 1',
+                  ),
+                  ClassCard(
+                    subject: 'Matemática',
+                    date: 'Sexta-feira 10:00 - 12:00',
+                    location: 'Aula 1',
+                  ),
+                  ClassCard(
+                    subject: 'Matemática',
+                    date: 'Quarta-feira 10:00 - 12:00',
+                    location: 'Aula 1',
+                  ),
+              ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
