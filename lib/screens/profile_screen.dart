eaf0fc7d1f2328d8ee86a8bf60f2957ce9370ce9
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-10 11:54:11 -0300

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,4 +1,6 @@
+import 'package:c317_mobile/components/user_avatar.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 class ProfileScreen extends StatelessWidget {
   const ProfileScreen({Key? key}) : super(key: key);
@@ -6,9 +8,52 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Perfil'),
+        body: Padding(
+      padding: const EdgeInsets.fromLTRB(24.0, 56.0, 24.0, 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              UserAvatar(),
+              IconButton(
+                onPressed: () {
+                  context.go('/login');
+                },
+                icon: const Icon(Icons.exit_to_app_rounded),
+                color: Theme.of(context).colorScheme.error,
+              ),
+            ],
+          ),
+          Text(
+            'Avner Joseph',
+            style: Theme.of(context).textTheme.displaySmall,
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+            child: Text(
+              'avner.joseph@gep.inatel.br',
+              style: Theme.of(context).textTheme.bodyMedium?.copyWith(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+            ),
+          ),
+          Text(
+            'GEP',
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+          ),
+          Text(
+            '1669',
+            style: Theme.of(context).textTheme.titleSmall?.copyWith(
+                  color: Theme.of(context).colorScheme.secondary,
+                ),
+          ),
+        ],
       ),
-    );
+    ));
   }
 }
