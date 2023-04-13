58f4cbd6303baae924837582a54ef9730a6dcbd8
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-13 15:13:33 -0300

    Update login_screen.dart

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -3,6 +3,8 @@ import 'package:c317_mobile/components/input_textfield.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../utils/validators.dart';
+
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
 
@@ -11,6 +13,9 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final GlobalKey<FormFieldState> emailFormKey = GlobalKey();
+  final GlobalKey<FormFieldState> passwordFormKey = GlobalKey();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,24 +50,35 @@ class _LoginScreenState extends State<LoginScreen> {
                 width: 100,
                 color: Theme.of(context).primaryColor,
               ),
-              const Padding(
-                padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+              Padding(
+                padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
                 child: InputTextField(
+                  formFieldKey: emailFormKey,
                   isPassword: false,
                   label: 'Entre com seu e-mail...',
                   keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    return Validators.validateEmail(value);
+                  },
                 ),
               ),
-              const InputTextField(
+              InputTextField(
+                formFieldKey: passwordFormKey,
                 isPassword: true,
                 label: 'Entre com sua senha...',
+                validator: (value) {
+                  return Validators.validatePassword(value);
+                },
               ),
               Padding(
                 padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
                 child: ActionButton(
                   text: 'Entrar',
                   onPressed: () {
-                    context.go('/onboarding');
+                    if (emailFormKey.currentState!.validate() &&
+                        passwordFormKey.currentState!.validate()) {
+                      context.go('/onboarding');
+                    }
                   },
                   color: Theme.of(context).primaryColor,
                   textColor: Theme.of(context).canvasColor,
