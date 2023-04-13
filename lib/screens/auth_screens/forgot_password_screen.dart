fb05d39a3660b3c83cfe9d75780cf620ca13e4b9
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-13 15:13:31 -0300

    Update forgot_password_screen.dart

--- lib/screens/auth_screens/forgot_password_screen.dart
+++ lib/screens/auth_screens/forgot_password_screen.dart
@@ -1,11 +1,12 @@
 import 'package:c317_mobile/components/action_button.dart';
 import 'package:c317_mobile/components/input_textfield.dart';
+import 'package:c317_mobile/utils/validators.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class ForgotPasswordScreen extends StatelessWidget {
-  const ForgotPasswordScreen({Key? key}) : super(key: key);
-
+  ForgotPasswordScreen({Key? key}) : super(key: key);
+  final GlobalKey<FormFieldState> emailFormKey = GlobalKey();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,17 +22,25 @@ class ForgotPasswordScreen extends StatelessWidget {
               'Digite seu e-mail para recuperar a senha',
               style: TextStyle(fontSize: 18),
             ),
-            const Padding(
-              padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
               child: InputTextField(
+                formFieldKey: emailFormKey,
                 isPassword: false,
                 label: 'Entre com seu e-mail...',
                 keyboardType: TextInputType.emailAddress,
+                validator: (value) {
+                  return Validators.validateEmail(value);
+                },
               ),
             ),
             ActionButton(
               text: 'Recuperar a senha',
-              onPressed: () => context.pop(),
+              onPressed: () {
+                if (emailFormKey.currentState!.validate()) {
+                  context.pop();
+                }
+              },
             )
           ],
         ),
