b3d71f42dd398982ba26f108a43d49eaff8acc42
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-20 11:31:42 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
+import 'package:c317_mobile/models/shared_preferences_provider.dart';
 import 'package:c317_mobile/routes/app_router.dart';
 import 'package:c317_mobile/themes/app_theme.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+  runApp(MultiProvider(providers: [
+    ChangeNotifierProvider(create: (_) => SharedPreferencesProvider()),
+  ], child: const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
