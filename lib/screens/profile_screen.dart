1152f3c29a3fe37007ea0db600325c933ba0235b
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-20 11:31:12 -0300

    Update profile_screen.dart

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,12 +1,27 @@
+import 'dart:io';
+
 import 'package:c317_mobile/components/user_avatar.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:provider/provider.dart';
 
-class ProfileScreen extends StatelessWidget {
+import '../controller/profile_picture_controller.dart';
+import '../models/shared_preferences_provider.dart';
+
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key? key}) : super(key: key);
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  final ImagePicker picker = ImagePicker();
+
+  @override
   Widget build(BuildContext context) {
+    final image = ProfilePictureController.getImage(context);
     return Scaffold(
         body: Padding(
       padding: const EdgeInsets.fromLTRB(24.0, 56.0, 24.0, 24.0),
@@ -17,9 +32,45 @@ class ProfileScreen extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              UserAvatar(),
+              (image != null)
+                  ? UserAvatar(imageUrl: image)
+                  : GestureDetector(
+                      onTap: () {
+                        picker
+                            .pickImage(
+                              source: ImageSource.gallery,
+                            )
+                            .then(
+                              (image) => {
+                                if (image != null)
+                                  {
+                                    setState(() {
+                                      ProfilePictureController.setImage(
+                                          File(image.path), context);
+                                    }),
+                                  }
+                              },
+                            );
+                      },
+                      child: Stack(
+                        alignment: Alignment.bottomRight,
+                        children: [
+                          const UserAvatar(),
+                          CircleAvatar(
+                            radius: 16,
+                            child: Icon(
+                              Icons.edit,
+                              color: Theme.of(context).primaryColor,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
               IconButton(
-                onPressed: () {
+                onPressed: () async {
+                  Provider.of<SharedPreferencesProvider>(context, listen: false)
+                      .prefs
+                      ?.remove('image');
                   context.replace('/login');
                 },
                 icon: const Icon(Icons.exit_to_app_rounded),
