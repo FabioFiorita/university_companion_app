5c2e38270fa489779f94a40a6567bc72d81e6e6d
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-25 15:56:58 -0300

    Update login_screen.dart

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -1,20 +1,24 @@
+import 'dart:io';
+
 import 'package:c317_mobile/components/action_button.dart';
 import 'package:c317_mobile/components/input_textfield.dart';
+import 'package:c317_mobile/service/auth_service.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:material_dialogs/dialogs.dart';
+import 'package:material_dialogs/widgets/buttons/icon_button.dart';
 
+import '../../exceptions/user_not_found_exception.dart';
 import '../../utils/validators.dart';
 
-class LoginScreen extends StatefulWidget {
-  const LoginScreen({Key? key}) : super(key: key);
+class LoginScreen extends StatelessWidget {
+  LoginScreen({Key? key}) : super(key: key);
 
-  @override
-  State<LoginScreen> createState() => _LoginScreenState();
-}
-
-class _LoginScreenState extends State<LoginScreen> {
   final GlobalKey<FormFieldState> emailFormKey = GlobalKey();
   final GlobalKey<FormFieldState> passwordFormKey = GlobalKey();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -54,6 +58,7 @@ class _LoginScreenState extends State<LoginScreen> {
                 padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
                 child: InputTextField(
                   formFieldKey: emailFormKey,
+                  controller: _emailController,
                   isPassword: false,
                   label: 'Entre com seu e-mail...',
                   keyboardType: TextInputType.emailAddress,
@@ -64,6 +69,7 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
               InputTextField(
                 formFieldKey: passwordFormKey,
+                controller: _passwordController,
                 isPassword: true,
                 label: 'Entre com sua senha...',
                 validator: (value) {
@@ -77,7 +83,7 @@ class _LoginScreenState extends State<LoginScreen> {
                   onPressed: () {
                     if (emailFormKey.currentState!.validate() &&
                         passwordFormKey.currentState!.validate()) {
-                      context.go('/onboarding');
+                      _tryLogin(context);
                     }
                   },
                   color: Theme.of(context).primaryColor,
@@ -96,5 +102,51 @@ class _LoginScreenState extends State<LoginScreen> {
         ),
       ),
     ));
+  }
+
+  _tryLogin(BuildContext context) async {
+    final AuthService authService = AuthService(context: context);
+    String email = _emailController.text;
+    String password = _passwordController.text;
+
+    authService.login(email, password).then((token) {
+      context.go('/onboarding');
+    }).catchError((e) {
+      Dialogs.materialDialog(
+        context: context,
+        title: "Usuário ainda não existe",
+        msg: "Verifique com a universidade se você já foi cadastrado.",
+        actions: [
+          IconsButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            text: "Ok",
+            color: Theme.of(context).primaryColor,
+            iconData: Icons.check_rounded,
+            textStyle: TextStyle(color: Theme.of(context).canvasColor),
+            iconColor: Theme.of(context).canvasColor,
+          ),
+        ],
+      );
+    }, test: (e) => e is UserNotFoundException).catchError((e) {
+      HttpException exception = e as HttpException;
+      Dialogs.materialDialog(
+          context: context,
+          title: "Erro",
+          msg: exception.message,
+          actions: [
+            IconsButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              text: "Ok",
+              color: Theme.of(context).primaryColor,
+              iconData: Icons.check_rounded,
+              textStyle: TextStyle(color: Theme.of(context).canvasColor),
+              iconColor: Theme.of(context).canvasColor,
+            ),
+          ]);
+    }, test: (e) => e is HttpException);
   }
 }
