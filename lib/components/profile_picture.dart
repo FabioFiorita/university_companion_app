aade7de613fe4f98936d653d79d12e8bb6f006c1
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-25 10:47:36 -0300

    Update profile_picture.dart

--- lib/components/profile_picture.dart
+++ lib/components/profile_picture.dart
@@ -1,29 +1,19 @@
 import 'package:c317_mobile/components/user_avatar.dart';
+import 'package:c317_mobile/providers/ProfilePictureProvider.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:provider/provider.dart';
-
-import '../state/profile_picture_store.dart';
 
 class ProfilePicture extends StatelessWidget {
   const ProfilePicture({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future:
-          Provider.of<ProfilePictureStore>(context, listen: false).loadImage(),
-      builder: (context, snapshot) {
-        return Observer(
-          builder: (_) {
-            final image =
-                Provider.of<ProfilePictureStore>(context, listen: false)
-                    .imageBytes;
-            return (image != null)
-                ? UserAvatar(imageUrl: image)
-                : const UserAvatar();
-          },
-        );
+    return Consumer<ProfilePictureProvider>(
+      builder: (_, store, __) {
+        final image = store.imageBytes;
+        return (image != null)
+            ? UserAvatar(imageUrl: image)
+            : const UserAvatar();
       },
     );
   }
