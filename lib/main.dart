3dc4a2f2d3f7292ebe5d094daabf6b5098e51d87
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-25 10:47:38 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,18 @@
+import 'package:c317_mobile/providers/ProfilePictureProvider.dart';
 import 'package:c317_mobile/routes/app_router.dart';
-import 'package:c317_mobile/state/profile_picture_store.dart';
 import 'package:c317_mobile/themes/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 void main() {
-  runApp(MultiProvider(providers: [
-    Provider<ProfilePictureStore>(
-      create: (_) => ProfilePictureStore(),
-    ),
-  ], child: const MyApp()));
+  runApp(MultiProvider(
+    providers: [
+      ChangeNotifierProvider<ProfilePictureProvider>(
+        create: (_) => ProfilePictureProvider(),
+      ),
+    ],
+    child: const MyApp(),
+  ));
 }
 
 class MyApp extends StatelessWidget {
