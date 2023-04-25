a468c45d8af605a0e5b0d889658a97a15adb2e6d
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-25 15:57:09 -0300

    Create web_client.dart

---
+++ lib/http/web_client.dart
@@ -0,0 +1,13 @@
+import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/intercepted_client.dart';
+
+import 'logging_interceptor.dart';
+
+class WebClient {
+  static const String baseUrl = 'http://192.168.68.124:3000/';
+
+  http.Client client = InterceptedClient.build(
+    interceptors: [LoggingInterceptor()],
+    requestTimeout: const Duration(seconds: 5),
+  );
+}
