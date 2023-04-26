a800194945e968b604474b8322b94b865334375f
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-26 18:00:22 -0300

    Update input_textfield.dart

--- lib/components/input_textfield.dart
+++ lib/components/input_textfield.dart
@@ -8,14 +8,22 @@ class InputTextField extends StatefulWidget {
   final String? Function(String?)? validator;
   final GlobalKey<FormFieldState> formFieldKey;
 
-  const InputTextField({Key? key, this.label, this.keyboardType, this.controller, required this.isPassword, this.validator, required this.formFieldKey}) : super(key: key);
+  const InputTextField(
+      {Key? key,
+      this.label,
+      this.keyboardType,
+      this.controller,
+      required this.isPassword,
+      this.validator,
+      required this.formFieldKey})
+      : super(key: key);
 
   @override
   State<InputTextField> createState() => _InputTextFieldState();
 }
 
 class _InputTextFieldState extends State<InputTextField> {
-  bool _showPassword = false;
+  bool _hidePassword = true;
 
   @override
   Widget build(BuildContext context) {
@@ -23,18 +31,20 @@ class _InputTextFieldState extends State<InputTextField> {
       key: widget.formFieldKey,
       controller: widget.controller,
       keyboardType: widget.keyboardType,
-      obscureText: widget.isPassword ? _showPassword : false,
+      obscureText: widget.isPassword ? _hidePassword : false,
       validator: widget.validator,
       decoration: InputDecoration(
-        suffixIcon: widget.isPassword ? InkWell(
-          onTap: () {
-            setState(() => _showPassword = !_showPassword);
-          },
-          borderRadius: BorderRadius.circular(10.0),
-          child: _showPassword
-              ? const Icon(Icons.visibility_off)
-              : const Icon(Icons.visibility),
-        ) : null,
+        suffixIcon: widget.isPassword
+            ? InkWell(
+                onTap: () {
+                  setState(() => _hidePassword = !_hidePassword);
+                },
+                borderRadius: BorderRadius.circular(10.0),
+                child: _hidePassword
+                    ? const Icon(Icons.visibility)
+                    : const Icon(Icons.visibility_off),
+              )
+            : null,
         border: const OutlineInputBorder(
           borderRadius: BorderRadius.all(Radius.circular(10.0)),
         ),
