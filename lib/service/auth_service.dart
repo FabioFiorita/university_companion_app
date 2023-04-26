ca421a8a51a95b9c36321392318f86ed2223d8d0
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-26 19:33:41 -0300

    Update auth_service.dart

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:c317_mobile/providers/user_provider.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
 
@@ -29,10 +29,10 @@ class AuthService {
       throw HttpException(response.body.toString());
     }
 
-    return saveInfosFromResponse(response.body);
+    return saveInfoFromResponse(response.body);
   }
 
-  Future<String> saveInfosFromResponse(String body) async {
+  Future<String> saveInfoFromResponse(String body) async {
     final String accessToken = json.decode(body)["accessToken"];
     final User user = User.fromJson(json.decode(body)["user"], accessToken);
     Provider.of<UserProvider>(context, listen: false).setUser(user);
