667236c765581c141873148084bc7d6acf0a9d49
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-26 19:33:47 -0300

    Update subjects_list_screen.dart

--- lib/screens/grades_screens/subjects_list_screen.dart
+++ lib/screens/grades_screens/subjects_list_screen.dart
@@ -1,13 +1,19 @@
+import 'package:c317_mobile/providers/subjects_provider.dart';
+import 'package:c317_mobile/service/subject_service.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../components/grades/subject_card.dart';
-
+import '../../models/subject.dart';
 
 class SubjectListScreen extends StatelessWidget {
   const SubjectListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final SubjectService subjectService = SubjectService(context: context);
+    final subjectProvider =
+        Provider.of<SubjectsProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Materias'),
@@ -22,18 +28,25 @@ class SubjectListScreen extends StatelessWidget {
               style: Theme.of(context).textTheme.labelSmall,
             ),
             Expanded(
-              child: ListView(
-                children: const [
-                  SubjectCard(
-                    subject: 'C317',
-                  ),
-                  SubjectCard(
-                    subject: 'H003',
-                  ),
-                  SubjectCard(
-                    subject: 'H004',
-                  ),
-                ],
+              child: FutureBuilder<bool>(
+                future: subjectProvider.getSubjects(context),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    final subjects = subjectProvider.subjects;
+                    return ListView.builder(
+                      itemCount: subjects.length,
+                      itemBuilder: (context, index) {
+                        return SubjectCard(
+                          subject: subjects[index].code,
+                        );
+                      },
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
               ),
             ),
           ],
