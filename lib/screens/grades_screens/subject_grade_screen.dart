9127945d8e48989258f549dd9dfe30ea336ec07e
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-27 19:35:19 -0300

    Update subject_grade_screen.dart

--- lib/screens/grades_screens/subject_grade_screen.dart
+++ lib/screens/grades_screens/subject_grade_screen.dart
@@ -1,43 +1,62 @@
 import 'package:c317_mobile/components/action_button.dart';
 import 'package:c317_mobile/components/grades/average_card.dart';
+import 'package:c317_mobile/components/grades/grade_bottom_sheet.dart';
 import 'package:c317_mobile/components/grades/grade_card.dart';
 import 'package:flutter/material.dart';
 
-class SubjectGradeScreen extends StatelessWidget {
+class SubjectGradeScreen extends StatefulWidget {
   final String subject;
 
-  SubjectGradeScreen({Key? key, required this.subject}) : super(key: key);
+  const SubjectGradeScreen({Key? key, required this.subject}) : super(key: key);
 
-  final Map<String, int?> grades = {'NP1': 80, 'NP2': 50, 'NP3': 10, 'NPL': 90};
+  @override
+  State<SubjectGradeScreen> createState() => _SubjectGradeScreenState();
+}
+
+class _SubjectGradeScreenState extends State<SubjectGradeScreen> {
+  Map<String, int?> grades = {'NP1': 80, 'NP2': 50, 'NP3': 10, 'NPL': 90};
+
+  final TextEditingController gradeNameController = TextEditingController();
+
+  final TextEditingController gradeValueController = TextEditingController();
+  bool isSimulating = false;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text(subject),
-        ),
-        body: Padding(
-          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-          child: CustomScrollView(
-            slivers: [
-              SliverToBoxAdapter(
-                child: AverageCard(grade: getAverage()),
-              ),
-              SliverToBoxAdapter(
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
-                  child: ActionButton(
-                      text: 'Simular',
-                      color: Theme.of(context).primaryColor,
-                      textColor: Theme.of(context).canvasColor,
-                      onPressed: () {}),
+      appBar: AppBar(
+        title: Text(widget.subject),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+        child: CustomScrollView(
+          slivers: [
+            SliverToBoxAdapter(
+              child: AverageCard(grade: getAverage()),
+            ),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(top: 8.0, bottom: 16.0),
+                child: ActionButton(
+                  text: (isSimulating == true) ? "Parar de simular" : 'Simular',
+                  color: Theme.of(context).primaryColor,
+                  textColor: Theme.of(context).canvasColor,
+                  onPressed: () {
+                    setState(
+                      () {
+                        isSimulating = !isSimulating;
+                        grades = {'NP1': 80, 'NP2': 50, 'NP3': 10, 'NPL': 90};
+                      },
+                    );
+                  },
                 ),
               ),
-              const SliverToBoxAdapter(
-                child: Text('Notas'),
-              ),
-              SliverList(
-                  delegate: SliverChildBuilderDelegate(
+            ),
+            const SliverToBoxAdapter(
+              child: Text('Notas'),
+            ),
+            SliverList(
+              delegate: SliverChildBuilderDelegate(
                 (context, index) {
                   return Padding(
                     padding: const EdgeInsets.only(top: 4.0),
@@ -48,10 +67,42 @@ class SubjectGradeScreen extends StatelessWidget {
                   );
                 },
                 childCount: grades.length,
-              )),
-            ],
-          ),
-        ));
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: (isSimulating == true)
+          ? FloatingActionButton(
+              onPressed: () {
+                showModalBottomSheet(
+                  context: context,
+                  isScrollControlled: true,
+                  builder: (context) {
+                    return GradeBottomSheet(
+                      onPressed: () {
+                        setState(
+                          () {
+                            grades.addEntries([
+                              MapEntry(
+                                gradeNameController.text,
+                                int.parse(gradeValueController.text),
+                              )
+                            ]);
+                            Navigator.pop(context);
+                          },
+                        );
+                      },
+                      gradeNameController: gradeNameController,
+                      gradeValueController: gradeValueController,
+                    );
+                  },
+                );
+              },
+              child: const Icon(Icons.add),
+            )
+          : Container(),
+    );
   }
 
   int getAverage() {
