b4f90367138efd9fbdfd0f4b4c21a81c68f93fb4
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-27 15:27:40 -0300

    Update login_screen.dart

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -2,13 +2,13 @@ import 'dart:io';
 
 import 'package:c317_mobile/components/action_button.dart';
 import 'package:c317_mobile/components/input_textfield.dart';
-import 'package:c317_mobile/service/auth_service.dart';
+import 'package:c317_mobile/providers/user_provider.dart';
+import 'package:c317_mobile/exceptions/login_exception.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
-import 'package:material_dialogs/dialogs.dart';
-import 'package:material_dialogs/widgets/buttons/icon_button.dart';
+import 'package:provider/provider.dart';
 
-import '../../exceptions/user_not_found_exception.dart';
+import '../../utils/material_dialog.dart';
 import '../../utils/validators.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -23,130 +23,114 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          maxHeight: MediaQuery.of(context).size.height,
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            children: [
-              const Padding(
-                padding: EdgeInsets.all(32.0),
-                child: Image(
-                  image: AssetImage('assets/images/logo.png'),
-                  height: 280,
-                  fit: BoxFit.cover,
+      body: SingleChildScrollView(
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            maxHeight: MediaQuery.of(context).size.height,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(32.0),
+                  child: Image(
+                    image: AssetImage('assets/images/logo.png'),
+                    height: 280,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(bottom: 4.0),
-                child: Text(
-                  'Login',
-                  style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                        color: Theme.of(context).primaryColor,
-                      ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 4.0),
+                  child: Text(
+                    'Login',
+                    style: Theme.of(context).textTheme.displaySmall?.copyWith(
+                          color: Theme.of(context).primaryColor,
+                        ),
+                  ),
                 ),
-              ),
-              Container(
-                height: 3,
-                width: 100,
-                color: Theme.of(context).primaryColor,
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-                child: InputTextField(
-                  formFieldKey: emailFormKey,
-                  controller: _emailController,
-                  isPassword: false,
-                  label: 'Entre com seu e-mail...',
-                  keyboardType: TextInputType.emailAddress,
-                  validator: (value) {
-                    return Validators.validateEmail(value);
-                  },
-                ),
-              ),
-              InputTextField(
-                formFieldKey: passwordFormKey,
-                controller: _passwordController,
-                isPassword: true,
-                label: 'Entre com sua senha...',
-                validator: (value) {
-                  return Validators.validatePassword(value);
-                },
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
-                child: ActionButton(
-                  text: 'Entrar',
-                  onPressed: () {
-                    if (emailFormKey.currentState!.validate() &&
-                        passwordFormKey.currentState!.validate()) {
-                      _tryLogin(context);
-                    }
-                  },
+                Container(
+                  height: 3,
+                  width: 100,
                   color: Theme.of(context).primaryColor,
-                  textColor: Theme.of(context).canvasColor,
                 ),
-              ),
-              ActionButton(
-                text: 'Esqueci minha senha',
-                onPressed: () {
-                  context.push('/forgot-password');
-                },
-                color: Theme.of(context).canvasColor,
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+                  child: InputTextField(
+                    formFieldKey: emailFormKey,
+                    controller: _emailController,
+                    isPassword: false,
+                    label: 'Entre com seu e-mail...',
+                    keyboardType: TextInputType.emailAddress,
+                    validator: (value) {
+                      return Validators.validateEmail(value);
+                    },
+                  ),
+                ),
+                InputTextField(
+                  formFieldKey: passwordFormKey,
+                  controller: _passwordController,
+                  isPassword: true,
+                  label: 'Entre com sua senha...',
+                  validator: (value) {
+                    return Validators.validatePassword(value);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                  child: ActionButton(
+                    text: 'Entrar',
+                    onPressed: () {
+                      if (emailFormKey.currentState!.validate() &&
+                          passwordFormKey.currentState!.validate()) {
+                        _tryLogin(context);
+                      }
+                    },
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).canvasColor,
+                  ),
+                ),
+                ActionButton(
+                  text: 'Esqueci minha senha',
+                  onPressed: () {
+                    context.push('/forgot-password');
+                  },
+                  color: Theme.of(context).canvasColor,
+                ),
+              ],
+            ),
           ),
         ),
       ),
-    ));
+    );
   }
 
   _tryLogin(BuildContext context) async {
-    final AuthService authService = AuthService(context: context);
+    final UserProvider userProvider =
+        Provider.of<UserProvider>(context, listen: false);
     String email = _emailController.text;
     String password = _passwordController.text;
 
-    authService.login(email, password).then((token) {
+    userProvider.login(email, password).then((_) {
       context.go('/onboarding');
-    }).catchError((e) {
-      Dialogs.materialDialog(
-        context: context,
-        title: "Usuário ainda não existe",
-        msg: "Verifique com a universidade se você já foi cadastrado.",
-        actions: [
-          IconsButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            text: "Ok",
-            color: Theme.of(context).primaryColor,
-            iconData: Icons.check_rounded,
-            textStyle: TextStyle(color: Theme.of(context).canvasColor),
-            iconColor: Theme.of(context).canvasColor,
-          ),
-        ],
-      );
-    }, test: (e) => e is UserNotFoundException).catchError((e) {
-      HttpException exception = e as HttpException;
-      Dialogs.materialDialog(
-          context: context,
-          title: "Erro",
-          msg: exception.message,
-          actions: [
-            IconsButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              text: "Ok",
-              color: Theme.of(context).primaryColor,
-              iconData: Icons.check_rounded,
-              textStyle: TextStyle(color: Theme.of(context).canvasColor),
-              iconColor: Theme.of(context).canvasColor,
-            ),
-          ]);
-    }, test: (e) => e is HttpException);
+    }).catchError(
+      (e) {
+        if (e is LoginException) {
+          MaterialDialog.showMaterialDialog(context, e.title, e.message);
+        } else if (e is SocketException) {
+          MaterialDialog.showMaterialDialog(
+            context,
+            'Erro de conexão',
+            'Verifique sua conexão com a internet.',
+          );
+        } else {
+          MaterialDialog.showMaterialDialog(
+            context,
+            'Erro desconhecido',
+            'Ocorreu um erro desconhecido. Tente novamente mais tarde.',
+          );
+        }
+      },
+    );
   }
 }
