590b9074652087fb4ef0d14433ac5fc47e628bd7
Fabio Fiorita <fabiolfp@gmail.com>
2023-04-28 10:56:56 -0300

    Update grade_bottom_sheet.dart

--- lib/components/grades/grade_bottom_sheet.dart
+++ lib/components/grades/grade_bottom_sheet.dart
@@ -8,55 +8,65 @@ class GradeBottomSheet extends StatelessWidget {
   final TextEditingController gradeNameController;
   final TextEditingController gradeValueController;
 
-  GradeBottomSheet(
+  const GradeBottomSheet(
       {Key? key,
       required this.onPressed,
       required this.gradeNameController,
       required this.gradeValueController})
       : super(key: key);
 
-  final GlobalKey<FormFieldState> gradeName = GlobalKey();
-  final GlobalKey<FormFieldState> gradeValue = GlobalKey();
+  static final GlobalKey<FormFieldState> gradeName = GlobalKey();
+  static final GlobalKey<FormFieldState> gradeValue = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          minHeight: MediaQuery.of(context).size.height * 0.4,
+      child: Padding(
+        padding: EdgeInsets.only(
+          left: 16.0,
+          right: 16.0,
+          top: 16.0,
+          bottom: MediaQuery.of(context).viewInsets.bottom,
         ),
-        child: Padding(
-          padding: EdgeInsets.fromLTRB(16.0, 16.0, 16.0,
-              MediaQuery.of(context).viewInsets.bottom + 16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              InputTextField(
-                controller: gradeNameController,
-                isPassword: false,
-                formFieldKey: gradeName,
-                label: "Avaliação",
-                validator: (value) {
-                  return Validators.validateTextField(value);
-                },
-              ),
-              InputTextField(
-                controller: gradeValueController,
-                isPassword: false,
-                formFieldKey: gradeValue,
-                label: "Nota",
-                validator: (value) {
-                  return Validators.validateTextField(value);
-                },
-              ),
-              ActionButton(
-                text: "Adicionar nota",
-                onPressed: onPressed,
-                color: Theme.of(context).primaryColor,
-                textColor: Theme.of(context).canvasColor,
-              )
-            ],
+        child: SingleChildScrollView(
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                InputTextField(
+                  controller: gradeNameController,
+                  isPassword: false,
+                  formFieldKey: gradeName,
+                  label: "Avaliação",
+                  validator: (value) {
+                    return Validators.validateTextField(value);
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+                  child: InputTextField(
+                    controller: gradeValueController,
+                    isPassword: false,
+                    formFieldKey: gradeValue,
+                    label: "Nota",
+                    keyboardType: TextInputType.number,
+                    validator: (value) {
+                      return Validators.validateTextField(value);
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 24.0),
+                  child: ActionButton(
+                    text: "Adicionar nota",
+                    onPressed: onPressed,
+                    color: Theme.of(context).primaryColor,
+                    textColor: Theme.of(context).canvasColor,
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
