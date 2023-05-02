fc50cf44e7d760dd92d786b5acb7844ff0fbd06a
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-02 11:18:18 -0300

    Update teacher.dart

--- lib/models/teacher.dart
+++ lib/models/teacher.dart
@@ -2,13 +2,11 @@ class Teacher {
   final int id;
   final String name;
   final String email;
-  final int userId;
 
   Teacher({
     required this.id,
     required this.name,
     required this.email,
-    required this.userId,
   });
 
   factory Teacher.fromJson(Map<String, dynamic> json) {
@@ -16,7 +14,6 @@ class Teacher {
       id: json['id'],
       name: json['name'],
       email: json['email'],
-      userId: json['userId'],
     );
   }
 }
