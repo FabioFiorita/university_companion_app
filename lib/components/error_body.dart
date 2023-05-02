e0f9570208c1868d8ab6b54e474b9bb472c601c3
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-02 10:50:02 -0300

    Create error_body.dart

---
+++ lib/components/error_body.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ErrorBody extends StatelessWidget {
+  final String? title;
+  final String? message;
+
+  const ErrorBody({Key? key, this.title, this.message}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          (title != null)
+              ? Text(
+                  title!,
+                  style: Theme.of(context).textTheme.headlineMedium,
+                )
+              : const Text('Erro inesperado'),
+          (message != null)
+              ? Text(
+                  message!,
+                  style: Theme.of(context).textTheme.bodyMedium,
+                )
+              : const Text('Tente novamente mais tarde.'),
+        ],
+      ),
+    );
+  }
+}
