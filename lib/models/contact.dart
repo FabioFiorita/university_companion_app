7278e1e495e551a93ce85e43218e66ca64dafad1
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-02 11:18:14 -0300

    Update contact.dart

--- lib/models/contact.dart
+++ lib/models/contact.dart
@@ -2,13 +2,11 @@ class Contact {
   final int id;
   final String area;
   final String number;
-  final int userId;
 
   Contact({
     required this.id,
     required this.area,
     required this.number,
-    required this.userId,
   });
 
   factory Contact.fromJson(Map<String, dynamic> json) {
@@ -16,7 +14,6 @@ class Contact {
       id: json['id'],
       area: json['area'],
       number: json['number'],
-      userId: json['userId'],
     );
   }
 }
