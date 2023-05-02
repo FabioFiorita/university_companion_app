05bcad1e194e34656b2558f3134eef92f37464e8
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-02 10:50:05 -0300

    Update web_client.dart

--- lib/http/web_client.dart
+++ lib/http/web_client.dart
@@ -4,6 +4,7 @@ import 'package:http_interceptor/http/intercepted_client.dart';
 import 'logging_interceptor.dart';
 
 class WebClient {
+  // static const String baseUrl = 'http://192.168.68.124:8000/api/';
   static const String baseUrl = 'http://192.168.68.124:3000/';
 
   http.Client client = InterceptedClient.build(
