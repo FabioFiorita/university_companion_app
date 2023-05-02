3eb8d4fa26e010e52a2241005a3f4eb909a48bdd
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-02 10:50:28 -0300

    Update subject_service.dart

--- lib/service/subject_service.dart
+++ lib/service/subject_service.dart
@@ -1,13 +1,12 @@
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:c317_mobile/exceptions/subject_exception.dart';
+import 'package:c317_mobile/exceptions/user_exception.dart';
 import 'package:c317_mobile/models/subject.dart';
-import 'package:c317_mobile/providers/subjects_provider.dart';
-import 'package:c317_mobile/providers/user_provider.dart';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 
+import '../exceptions/general_exception.dart';
 import '../http/web_client.dart';
 import '../models/user.dart';
 
@@ -15,21 +14,25 @@ class SubjectService {
   http.Client client = WebClient().client;
 
   Future<List<Subject>> getSubjects(User user) async {
-    http.Response response = await client.get(
-      Uri.parse("${WebClient.baseUrl}users/${user.id}/subjects"),
-      headers: {
-        "Authorization": "Bearer ${user.accessToken}",
-      },
-    ).timeout(const Duration(seconds: 5));
+    try {
+      http.Response response = await client.get(
+          Uri.parse("${WebClient.baseUrl}users/${user.id}/subjects"),
+          headers: {
+            "Authorization": "Bearer ${user.accessToken}",
+          }).timeout(const Duration(seconds: 5));
+      // Uri.parse("${WebClient.baseUrl}subject"),
+      // headers: {
+      //   "content-type": "application/json",
+      // }).timeout(const Duration(seconds: 5));
 
-    if (response.statusCode != 200) {
-      if (json.decode(response.body).toString() == "Cannot find subjects") {
-        throw const HttpException("Cannot find subjects");
+      if (response.statusCode != 200) {
+        _handleStatusCode(response);
       }
-      throw HttpException(response.body.toString());
-    }
 
-    return saveInfoFromResponse(response.body);
+      return saveInfoFromResponse(response.body);
+    } catch (e) {
+      rethrow;
+    }
   }
 
   Future<List<Subject>> saveInfoFromResponse(String body) async {
@@ -39,5 +42,23 @@ class SubjectService {
         .toList();
 
     return subjects;
+  }
+
+  void _handleStatusCode(http.Response response) {
+    switch (response.statusCode) {
+      case 200:
+        // success
+        break;
+      case 400:
+        throw GeneralException.undefined;
+      case 401:
+        throw UserException.sessionExpired;
+      case 404:
+        throw SubjectException.subjectNotFound;
+      case 429:
+        throw GeneralException.tooManyRequests;
+      default:
+        throw GeneralException.undefined;
+    }
   }
 }
