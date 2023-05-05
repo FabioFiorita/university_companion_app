d07b46b36cc07da9c50dc4619b19be7f26a0da29
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-05 11:51:55 -0300

    Update subject.dart

--- lib/models/subject.dart
+++ lib/models/subject.dart
@@ -1,25 +1,24 @@
+import 'package:c317_mobile/models/teacher.dart';
+
 class Subject {
   final int id;
   final String name;
   final String code;
-  final int teacherId;
-  final int userId;
+  final Teacher teacher;
 
   Subject({
     required this.id,
     required this.name,
     required this.code,
-    required this.teacherId,
-    required this.userId,
+    required this.teacher,
   });
 
-  factory Subject.fromJson(Map<String, dynamic> json) {
+  factory Subject.fromJson(Map<String, dynamic> json, Teacher teacher) {
     return Subject(
       id: json['id'],
       name: json['name'],
       code: json['code'],
-      teacherId: json['teacherId'],
-      userId: json['userId'],
+      teacher: teacher,
     );
   }
 }
