9d9a0d168a77c83b3252b43676418cdf521c57f7
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-05 11:51:35 -0300

    Update class_exception.dart

--- lib/exceptions/class_exception.dart
+++ lib/exceptions/class_exception.dart
@@ -1,6 +1,6 @@
 enum ClassException implements Exception {
   classNotFound(
-      title: "Turma não encontrada",
+      title: "Aulas não encontradas",
       message: "Ocorreu um erro ao buscar as turmas.");
 
   const ClassException({required this.title, required this.message});
