2fbc06d67304e6dbe4aec5f0114484a0f108265e
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-05 11:51:53 -0300

    Update grade.dart

--- lib/models/grade.dart
+++ lib/models/grade.dart
@@ -1,25 +1,24 @@
+import 'package:c317_mobile/models/subject.dart';
+
 class Grade {
   final int id;
   final String code;
   final int grade;
-  final int subjectId;
-  final int userId;
+  final Subject subject;
 
   Grade({
     required this.id,
     required this.code,
     required this.grade,
-    required this.subjectId,
-    required this.userId,
+    required this.subject,
   });
 
-  factory Grade.fromJson(Map<String, dynamic> json) {
+  factory Grade.fromJson(Map<String, dynamic> json, Subject subject) {
     return Grade(
       id: json['id'],
       code: json['code'],
       grade: json['grade'],
-      subjectId: json['subjectId'],
-      userId: json['userId'],
+      subject: subject,
     );
   }
 }
