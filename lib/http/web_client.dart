d49c8f97b8720abeb3f9cc5374862f2666186117
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-07 20:48:39 -0300

    Update web_client.dart

--- lib/http/web_client.dart
+++ lib/http/web_client.dart
@@ -5,7 +5,8 @@ import 'logging_interceptor.dart';
 
 class WebClient {
   // static const String baseUrl = 'http://192.168.68.124:8000/api/';
-  static const String baseUrl = 'http://192.168.100.124:3000/';
+  static const String baseUrl =
+      'https://json-server-vercel-icgvgk67x-fabiofiorita.vercel.app/';
 
   http.Client client = InterceptedClient.build(
     interceptors: [LoggingInterceptor()],
