f54f9224e312d042090977f0bb38d2aefadd2618
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-08 11:46:35 -0300

    Update grade_provider.dart

--- lib/providers/grade_provider.dart
+++ lib/providers/grade_provider.dart
@@ -3,6 +3,7 @@ import 'package:flutter/cupertino.dart';
 
 import '../exceptions/user_exception.dart';
 import '../models/grade.dart';
+import '../models/subject.dart';
 import '../models/user.dart';
 import '../service/grade_service.dart';
 
@@ -10,6 +11,8 @@ class GradeProvider extends ChangeNotifier {
   List<Grade> _grades = [];
 
   List<Grade> get grades => _grades;
+
+  List<Grade> simulatedGrades = [];
 
   final User? user;
 
@@ -54,5 +57,28 @@ class GradeProvider extends ChangeNotifier {
   Future<void> resetCache() async {
     _grades = [];
     await getGrades();
+  }
+
+  toggleGradeSimulation() {
+    simulatedGrades = _grades.toList();
+    notifyListeners();
+  }
+
+  bool addSimulatedGrade(String code, int value, Subject subject) {
+    if (code.isEmpty || value < 0 || value > 100) {
+      return false;
+    }
+    if (_grades.any((grade) => grade.code == code)) {
+      return false;
+    }
+    final Grade newGrade = Grade(
+      id: 0,
+      code: code,
+      grade: value,
+      subject: subject,
+    );
+    simulatedGrades.add(newGrade);
+    notifyListeners();
+    return true;
   }
 }
