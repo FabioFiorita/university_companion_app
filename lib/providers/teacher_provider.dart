585f8043bd279caaead318cb7163e2c9ac196233
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-08 10:04:55 -0300

    Update teacher_provider.dart

--- lib/providers/teacher_provider.dart
+++ lib/providers/teacher_provider.dart
@@ -9,24 +9,42 @@ class TeacherProvider extends ChangeNotifier {
 
   List<Teacher> get teachers => _teachers;
 
+  bool _isLoading = false;
+
+  bool get isLoading => _isLoading;
+
+  Object? _error;
+
+  Object? get error => _error;
+
   TeacherProvider() {
     getTeachers();
   }
 
   Future<void> getTeachers() async {
+    _isLoading = true;
     if (_teachers.isNotEmpty) {
+      _isLoading = false;
       return;
     }
     final TeacherService teacherService = TeacherService();
     try {
       final List<Teacher> teachers = await teacherService.getTeachers();
+      _isLoading = false;
       if (teachers.isEmpty) {
         throw TeacherException.teacherNotFound;
       }
       _teachers = teachers;
       notifyListeners();
     } catch (e) {
-      rethrow;
+      _error = e;
+      _isLoading = false;
+      notifyListeners();
     }
+  }
+
+  Future<void> cleanCache() async {
+    _teachers = [];
+    await getTeachers();
   }
 }
