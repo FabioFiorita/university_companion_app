f6eb5beaa9aab7ca93c91fa9169affb43ece7c99
Fabio Fiorita <fabiolfp@gmail.com>
2023-05-11 08:26:49 -0300

    Update login_screen.dart

--- lib/screens/auth_screens/login_screen.dart
+++ lib/screens/auth_screens/login_screen.dart
@@ -24,83 +24,97 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
-        child: ConstrainedBox(
-          constraints: BoxConstraints(
-            maxHeight: MediaQuery.of(context).size.height,
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                const Padding(
-                  padding: EdgeInsets.all(32.0),
-                  child: Image(
-                    image: AssetImage('assets/images/logo.png'),
-                    height: 280,
-                    fit: BoxFit.cover,
-                  ),
+        child:
+            Consumer<UserProvider>(builder: (_, UserProvider userProvider, __) {
+          return Stack(alignment: Alignment.center, children: [
+            (userProvider.isLoading)
+                ? const Center(child: CircularProgressIndicator())
+                : Container(),
+            Opacity(
+              opacity: (userProvider.isLoading) ? 0.5 : 1,
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  maxHeight: MediaQuery.of(context).size.height,
                 ),
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 4.0),
-                  child: Text(
-                    'Login',
-                    style: Theme.of(context).textTheme.displaySmall?.copyWith(
-                          color: Theme.of(context).primaryColor,
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(32.0),
+                        child: Image(
+                          image: AssetImage('assets/images/logo.png'),
+                          height: 280,
+                          fit: BoxFit.cover,
                         ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(bottom: 4.0),
+                        child: Text(
+                          'Login',
+                          style: Theme.of(context)
+                              .textTheme
+                              .displaySmall
+                              ?.copyWith(
+                                color: Theme.of(context).primaryColor,
+                              ),
+                        ),
+                      ),
+                      Container(
+                        height: 3,
+                        width: 100,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
+                        child: InputTextField(
+                          formFieldKey: emailFormKey,
+                          controller: _emailController,
+                          isPassword: false,
+                          label: 'Entre com seu e-mail...',
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) {
+                            return Validators.validateEmail(value);
+                          },
+                        ),
+                      ),
+                      InputTextField(
+                        formFieldKey: passwordFormKey,
+                        controller: _passwordController,
+                        isPassword: true,
+                        label: 'Entre com sua senha...',
+                        validator: (value) {
+                          return Validators.validatePassword(value);
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
+                        child: ActionButton(
+                          text: 'Entrar',
+                          onPressed: () {
+                            if (emailFormKey.currentState!.validate() &&
+                                passwordFormKey.currentState!.validate()) {
+                              _tryLogin(context);
+                            }
+                          },
+                          color: Theme.of(context).primaryColor,
+                          textColor: Theme.of(context).canvasColor,
+                        ),
+                      ),
+                      ActionButton(
+                        text: 'Esqueci minha senha',
+                        onPressed: () {
+                          context.push('/forgot-password');
+                        },
+                        color: Theme.of(context).canvasColor,
+                      ),
+                    ],
                   ),
                 ),
-                Container(
-                  height: 3,
-                  width: 100,
-                  color: Theme.of(context).primaryColor,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0, bottom: 16.0),
-                  child: InputTextField(
-                    formFieldKey: emailFormKey,
-                    controller: _emailController,
-                    isPassword: false,
-                    label: 'Entre com seu e-mail...',
-                    keyboardType: TextInputType.emailAddress,
-                    validator: (value) {
-                      return Validators.validateEmail(value);
-                    },
-                  ),
-                ),
-                InputTextField(
-                  formFieldKey: passwordFormKey,
-                  controller: _passwordController,
-                  isPassword: true,
-                  label: 'Entre com sua senha...',
-                  validator: (value) {
-                    return Validators.validatePassword(value);
-                  },
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(top: 16.0, bottom: 8.0),
-                  child: ActionButton(
-                    text: 'Entrar',
-                    onPressed: () {
-                      if (emailFormKey.currentState!.validate() &&
-                          passwordFormKey.currentState!.validate()) {
-                        _tryLogin(context);
-                      }
-                    },
-                    color: Theme.of(context).primaryColor,
-                    textColor: Theme.of(context).canvasColor,
-                  ),
-                ),
-                ActionButton(
-                  text: 'Esqueci minha senha',
-                  onPressed: () {
-                    context.push('/forgot-password');
-                  },
-                  color: Theme.of(context).canvasColor,
-                ),
-              ],
+              ),
             ),
-          ),
-        ),
+          ]);
+        }),
       ),
     );
   }
